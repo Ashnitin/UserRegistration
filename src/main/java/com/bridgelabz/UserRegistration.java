ba0cee692896937a2499d3f29bc5eb81958751ea
unknown <ashwinijadhav129aj@gmail.com>
2023-11-06 20:53:49 +0530

    [Added]:user need to enter valid email

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -4,16 +4,51 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    private String userInput;
+    private String firstName;
+    private String lastName;
+    private String email;
+
     public UserRegistration() {
     }
 
 
-    public boolean validFirstName(String userInput) {
-        Pattern p =Pattern.compile("[A-Z][A-Za-z]{2,}");
-        Matcher m=p.matcher(userInput);
-        boolean b=m.matches();
-        System.out.println(b);
-        return b;
+    public boolean validFirstName(String firstName)  {
+        try {
+            if(Pattern.matches("[A-Z][A-Za-z]{2,}",firstName)) {
+                return true;
+            }
+            else{
+                throw new UserRegistrationException("Enter Valid FirstName");
+            }
+        }
+        catch (UserRegistrationException e){
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+    public boolean validLastName(String lastName) {
+        try {
+            if (Pattern.matches("[A-Z][A-Za-z]{2,}", lastName)) {
+                return true;
+            } else {
+                throw new UserRegistrationException("Enter Valid LasttName");
+            }
+        } catch (UserRegistrationException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean validEmail(String email) {
+        try {
+            if (Pattern.matches("[a-z A-Z 0-9 _/-/.]+[@][a-z]+[/.][a-z]{2,3}",email )) {
+                return true;
+            } else {
+                throw new UserRegistrationException("Enter Valid Email");
+            }
+        } catch (UserRegistrationException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -4,12 +4,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserRegistrationTest {
+    UserRegistration ob=new UserRegistration();
     @Test
     public void givenValidFirstName(){
-        UserRegistration ob=new UserRegistration();
-        boolean firstName=ob.validFirstName("AshWini");
+        boolean firstName=ob.validFirstName("Ash");
         Assertions.assertTrue(firstName);
     }
-
+    @Test
+    public void givenValidLastName(){
+        boolean lastName=ob.validLastName("Bankar");
+        Assertions.assertTrue(lastName);
+    }
+    @Test
+    public void givenValidEmail(){
+        boolean email=ob.validEmail("ashwini.bankar11@gmail.com");
+        Assertions.assertTrue(email);
+    }
 
 }
