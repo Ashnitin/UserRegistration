f4909d62b2738a2098cb0ff91376b79a19db3dcd
unknown <ashwinijadhav129aj@gmail.com>
2023-11-06 13:36:26 +0530

    Welcome user registration

---
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class UserRegistration {
+    private String userInput;
+    public UserRegistration() {
+    }
+
+    public UserRegistration(String userInput) {
+        this.userInput = userInput;
+    }
+}
---
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+    @Test
+    public static void main(String[] args) {
+        System.out.println("Welcome UserRegistration");
+    }
+}
