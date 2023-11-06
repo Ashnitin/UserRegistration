e0c4c93f5c7d9f0ffa8820bf0f299107ecba9cf7
unknown <ashwinijadhav129aj@gmail.com>
2023-11-06 14:54:03 +0530

    [Added]:User need to Enter Valid first name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,11 +1,19 @@
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     private String userInput;
     public UserRegistration() {
     }
 
-    public UserRegistration(String userInput) {
-        this.userInput = userInput;
+
+    public boolean validFirstName(String userInput) {
+        Pattern p =Pattern.compile("[A-Z][A-Za-z]{2,}");
+        Matcher m=p.matcher(userInput);
+        boolean b=m.matches();
+        System.out.println(b);
+        return b;
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,10 +1,15 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserRegistrationTest {
     @Test
-    public static void main(String[] args) {
-        System.out.println("Welcome UserRegistration");
+    public void givenValidFirstName(){
+        UserRegistration ob=new UserRegistration();
+        boolean firstName=ob.validFirstName("AshWini");
+        Assertions.assertTrue(firstName);
     }
+
+
 }
